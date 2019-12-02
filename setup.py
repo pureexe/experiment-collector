d63a3db72bdaddce7fdcee77333f51030408d874
Pakkapon Phongthawee <me@pureapp.in.th>
2019-12-02 23:24:32 +0700

    remove comment from previous version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="experimentcollector", # Replace with your own username
+    name="experimentcollector",
     version="0.1.0",
     author="Pakkapon Phongthawee",
     author_email="pakkapon.p_s19@vistec.ac.th",
